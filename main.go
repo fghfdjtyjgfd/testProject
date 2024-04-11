3a3fcd7ee3fd206697eac142863e14b4f5b30887
fghfdjtyjgfd <fghfdjtyjgfd@hotmail.com>
2024-04-11 21:39:25 +0700

    add fiber API and method GET,POST,PUT,DELETE

--- main.go
+++ main.go
@@ -1,30 +1,73 @@
 package main
 
-import(
-	"fmt"
-	"gorm.io/gorm"
-	"gorm.io/driver/mysql"
-	
+import (
+	"strconv"
 
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
 	dsn := "root:root@tcp(127.0.0.1:3306)/testdb"
-	db, err := gorm.Open(mysql.Open(dsn),&gorm.Config{})
-	if err != nil{
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
 		panic("failed to connect database")
 	}
 	db.AutoMigrate(&Beer{})
-	fmt.Println("connected successful")
 
-	// newBeer := &Beer{
-	// 	ID: 1,
-	// 	Name: "cbg",
-	// 	Type: "good",
-	// 	Detail: "test",
-	// 	ImageURL: "google",
-	// }
-	// CreateBeer(db, newBeer)
-	currentBeer := SearchBeer(db, "cbg")
-	fmt.Println(currentBeer)
+	app := fiber.New()
+
+	app.Get("/beers", func(c *fiber.Ctx) error {
+		return c.JSON(GetBeers(db))
+	})
+
+	app.Get("/beers/:id", func(c *fiber.Ctx) error {
+		id, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		beer := GetBeer(db, id)
+		return c.JSON(beer)
+	})
+
+	app.Post("/beers", func(c *fiber.Ctx) error {
+		beer := new(Beer)
+		if err := c.BodyParser(beer); err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		if err := CreateBeer(db, beer); err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		return c.JSON(fiber.Map{"message": "created beer successful"})
+	})
+
+	app.Put("/beers/:id", func(c *fiber.Ctx) error {
+		beer := new(Beer)
+		id, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		if err := c.BodyParser(beer); err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		beer.ID = id
+		if err := UpdateBeer(db, beer); err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		return c.JSON(fiber.Map{"message": "updated beer successful"})
+	})
+
+	app.Delete("/beers/:id", func(c *fiber.Ctx) error {
+		id, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		if err := DeleteBeer(db, id); err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		return c.JSON(fiber.Map{"message": "deleted beer successful"})
+	})
+
+	app.Listen(":8000")
 }
--- model.go
+++ model.go
@@ -1,29 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"gorm.io/gorm"
 )
 
 type Beer struct {
-	ID uint
-	Name string
-	Type string
-	Detail string
+	ID       int
+	Name     string
+	Type     string
+	Detail   string
 	ImageURL string
 }
 
-func CreateBeer(db *gorm.DB, beer *Beer) {
+func CreateBeer(db *gorm.DB, beer *Beer) error {
 	result := db.Create(beer)
 	if result.Error != nil {
-		log.Fatalf("Error to create beer: %v", result.Error)
+		return result.Error
 	}
-	fmt.Println("Created beer successful")
+	return nil
 }
 
-func GetBeer(db *gorm.DB, id uint) *Beer{
+func GetBeers(db *gorm.DB) []Beer {
+	var beers []Beer
+	result := db.Find(&beers)
+	if result.Error != nil {
+		log.Fatalf("Error to get beer: %v", result.Error)
+	}
+	return beers
+}
+
+func GetBeer(db *gorm.DB, id int) *Beer {
 	var beer Beer
 	result := db.First(&beer, id)
 	if result.Error != nil {
@@ -32,30 +40,29 @@ func GetBeer(db *gorm.DB, id uint) *Beer{
 	return &beer
 }
 
-func UpdateBeer (db *gorm.DB, beer *Beer) {
-	
+func UpdateBeer(db *gorm.DB, beer *Beer) error {
 	result := db.Save(&beer)
 	if result.Error != nil {
-		log.Fatalf("Error to update beer: %v", result.Error)
+		return result.Error
 	}
-	fmt.Println("updated successful")
+	return nil
 }
 
-func DeleteBeer (db *gorm.DB, id uint){
+func DeleteBeer(db *gorm.DB, id int) error {
 	var beer Beer
-	result := db.Delete(&beer ,id)
+	result := db.Delete(&beer, id)
 	if result.Error != nil {
-		log.Fatalf("Error to delete beer: %v", result.Error)
+		return result.Error
 	}
-	fmt.Println("deleted successful")
+	return nil
 }
 
-func SearchBeer (db *gorm.DB, beerName string) *Beer{
+func SearchBeer(db *gorm.DB, beerName string) *Beer {
 	var beer Beer
 	result := db.Where("name = ?", beerName).First(&beer)
 	if result.Error != nil {
 		log.Fatalf("Error to search beer: %v", result.Error)
 	}
 	return &beer
-	
+
 }
